d5b1ad4c5da029ab4d716a540f8df4ce254149fd
Shammi2k <shammi2k@gmail.com>
2024-04-17 23:47:17 +0530

    List repositories in an organization

--- index.js
+++ index.js
@@ -21,15 +21,20 @@ const getToken = () => {
 const getUsersJSON = async () => {
     if (!userData)
     {
-        const { data } = await octokit.request("/user");
-        userData = data;
+        const response = await octokit.request("/user");
+        userData = response;
     }
     return userData;
 }
 
 const getOrganizationsJSON = async () => {
-    const {data } = await octokit.request('GET /user/orgs')
-    return data;
+    const response = await octokit.request('GET /user/orgs')
+    return response;
+}
+
+const getRepositoriesJSON = async (orgName, page) => {
+    const response = await octokit.request(`GET /orgs/${orgName}/repos?page=${page}`);
+    return response;
 }
 
 const octokit = new Octokit({ 
@@ -40,22 +45,60 @@ const octokit = new Octokit({
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-    const userInfo = await getUsersJSON();
+    const userInfo = (await getUsersJSON()).data;
     const response = {};
     response.user = {};
     response.user.name = userInfo.name;
     response.user.link = userInfo.html_url;
 
-    const organizations = await getOrganizationsJSON();
+    const organizations = (await getOrganizationsJSON()).data;
     response.organizations = organizations;
     res.render("index.ejs", response);
 });
 
-app.get("/orgs/*", async (req, res) => {
-    const userInfo = await getUsersJSON();
+app.get("/orgs/*/repositories", async (req, res) => {
+    const userInfo = (await getUsersJSON()).data;
     const response = {};
+    response.pagination = true;
     response.user = {};
     response.user.name = userInfo.name;
+
+    var orgName = req.url.split("/orgs/")[1].split("/repositories")[0];
+    response.orgName = orgName;
+    var page = 1;
+    if (req.query.page)
+    {
+        page = parseInt(req.query.page);
+    }
+    console.log("Page : " + page);
+    response.page = page;
+    const repos = await getRepositoriesJSON(orgName, page);
+    const linkHeader = repos.headers.link;
+    const hasNextPage = linkHeader && linkHeader.includes(`rel=\"next\"`);
+    const hasPrevPage = linkHeader && linkHeader.includes(`rel=\"prev\"`);
+    if (hasNextPage) {
+      response.nextPage = linkHeader.split(">; rel=\"next\"")[0].split("?page=").pop();
+      response.lastPage = linkHeader.split(">; rel=\"last\"")[0].split("?page=").pop();
+    }
+    if (hasPrevPage) {
+        response.prevPage = linkHeader.split(">; rel=\"prev\"")[0].split("?page=").pop();
+        response.firstPage = linkHeader.split(">; rel=\"first\"")[0].split("?page=").pop();
+      }
+    response.repos = repos.data;
+    res.render("reposList.ejs", response);
+});
+
+app.get("/orgs/*", async (req, res) => {
+    const userInfo = (await getUsersJSON()).data;
+    const response = {};
+    response.showAllRepos = true;
+    response.user = {};
+    response.user.name = userInfo.name;
+
+    var orgName = req.url.split("/orgs/")[1];
+    response.orgName = orgName;
+    const repos = await getRepositoriesJSON(orgName, 1);
+    response.repos = repos.data;
     res.render("org.ejs", response);
 });
 
